97b5e2f791a2157cd960e0f18b50d266b4536fc6
karishma Yadav <karishmay.enjay@gmail.com>
2025-02-26 20:01:31 +0530

    Initial Commit

---
+++ LevelsupermindiosAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LevelsupermindiosAssignment
+//
+//  Created by enjay on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
